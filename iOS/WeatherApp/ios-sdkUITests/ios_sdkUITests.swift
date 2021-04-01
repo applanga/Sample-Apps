1ddb9b49465107b96ea556465931732d7452940f
yoav <ybenzvi@applanga.com>
2021-04-01 16:21:31 +0300

    working ui test

--- iOS/WeatherApp/ios-sdkUITests/ios_sdkUITests.swift
+++ iOS/WeatherApp/ios-sdkUITests/ios_sdkUITests.swift
@@ -18,59 +18,111 @@ class ios_sdkUITests: XCTestCase {
         app.launch()
     }
     
-    func testExample0() {
+    func testExample() {
         let hamburgerBtn = app.buttons["hamburgerBtn"]
         let dailyBtn = app.buttons["navDaily"]
         let aboutBtn = app.buttons["navAbout"]
         let settingsBtn = app.buttons["navSettings"]
         
-        // home
         sleep(2)
-        // ! take screenshot !
-        //open screenshot menu by tapping invisible Applanga button
+        
+        // home
+        // take screenshot
+        // open screenshot menu by tapping invisible Applanga button
         app.buttons["Applanga.ToggleDraftMenu"].coordinate(withNormalizedOffset: CGVector(dx: 0.5, dy: 0.5)).tap()
         app.buttons["Applanga.OpenScreenshotView"].coordinate(withNormalizedOffset: CGVector(dx: 0.5, dy: 0.5)).tap()
         app.buttons["Applanga.SelectTag"].coordinate(withNormalizedOffset: CGVector(dx: 0.5, dy: 0.5)).tap()
-        app.tables.staticTexts["Test"].tap();
+        // select tag from tag list
+        app.tables.staticTexts["Home"].tap();
         app.buttons["Applanga.ConfirmScreenshot"].coordinate(withNormalizedOffset: CGVector(dx: 0.5, dy: 0.5)).tap()
 
-        //screenshot upload takes a while so we need to wait until the screenshot menu is visible again until we can proceed
-        let predicate = NSPredicate(format: "exists == 1")
-        let query = XCUIApplication().buttons["Applanga.SelectTag"];
+        // screenshot upload takes a while so we need to wait until the screenshot menu is visible again until we can proceed
+        var predicate = NSPredicate(format: "exists == 1")
+        var query = XCUIApplication().buttons["Applanga.SelectTag"];
         expectation(for: predicate, evaluatedWith: query, handler: nil)
         waitForExpectations(timeout: 30, handler: nil)
         
-        // hide
+        // hide screenshot menu
         app.buttons["Applanga.CancelScreenshot"].coordinate(withNormalizedOffset: CGVector(dx: 0.5, dy: 0.5)).tap()
         app.buttons["Applanga.ToggleDraftMenu"].coordinate(withNormalizedOffset: CGVector(dx: 0.5, dy: 0.5)).tap()
         
-        sleep(2)
-        print("Should take screenshot")
         // daily
         hamburgerBtn.tap()
         sleep(1)
         dailyBtn.tap()
         sleep(1)
-        // ! take screenshot !
+        // take screenshot
+        app.buttons["Applanga.ToggleDraftMenu"].coordinate(withNormalizedOffset: CGVector(dx: 0.5, dy: 0.5)).tap()
+        app.buttons["Applanga.OpenScreenshotView"].coordinate(withNormalizedOffset: CGVector(dx: 0.5, dy: 0.5)).tap()
+        app.buttons["Applanga.SelectTag"].coordinate(withNormalizedOffset: CGVector(dx: 0.5, dy: 0.5)).tap()
+        app.tables.staticTexts["Daily"].tap();
+        app.buttons["Applanga.ConfirmScreenshot"].coordinate(withNormalizedOffset: CGVector(dx: 0.5, dy: 0.5)).tap()
+
+        predicate = NSPredicate(format: "exists == 1")
+        query = XCUIApplication().buttons["Applanga.SelectTag"];
+        expectation(for: predicate, evaluatedWith: query, handler: nil)
+        waitForExpectations(timeout: 30, handler: nil)
+        
+        app.buttons["Applanga.CancelScreenshot"].coordinate(withNormalizedOffset: CGVector(dx: 0.5, dy: 0.5)).tap()
+        app.buttons["Applanga.ToggleDraftMenu"].coordinate(withNormalizedOffset: CGVector(dx: 0.5, dy: 0.5)).tap()
         
         // about
         hamburgerBtn.tap()
         sleep(1)
         aboutBtn.tap()
         sleep(3)
-        // ! take screenshot !
+        // take screenshot
+        app.buttons["Applanga.ToggleDraftMenu"].coordinate(withNormalizedOffset: CGVector(dx: 0.5, dy: 0.5)).tap()
+        app.buttons["Applanga.OpenScreenshotView"].coordinate(withNormalizedOffset: CGVector(dx: 0.5, dy: 0.5)).tap()
+        app.buttons["Applanga.SelectTag"].coordinate(withNormalizedOffset: CGVector(dx: 0.5, dy: 0.5)).tap()
+        app.tables.staticTexts["About"].tap();
+        app.buttons["Applanga.ConfirmScreenshot"].coordinate(withNormalizedOffset: CGVector(dx: 0.5, dy: 0.5)).tap()
+
+        predicate = NSPredicate(format: "exists == 1")
+        query = XCUIApplication().buttons["Applanga.SelectTag"];
+        expectation(for: predicate, evaluatedWith: query, handler: nil)
+        waitForExpectations(timeout: 30, handler: nil)
+        
+        app.buttons["Applanga.CancelScreenshot"].coordinate(withNormalizedOffset: CGVector(dx: 0.5, dy: 0.5)).tap()
+        app.buttons["Applanga.ToggleDraftMenu"].coordinate(withNormalizedOffset: CGVector(dx: 0.5, dy: 0.5)).tap()
         
         // settings
         hamburgerBtn.tap()
         sleep(1)
         settingsBtn.tap()
         sleep(1)
-        // ! take screenshot !
+        // take screenshot
+        app.buttons["Applanga.ToggleDraftMenu"].coordinate(withNormalizedOffset: CGVector(dx: 0.5, dy: 0.5)).tap()
+        app.buttons["Applanga.OpenScreenshotView"].coordinate(withNormalizedOffset: CGVector(dx: 0.5, dy: 0.5)).tap()
+        app.buttons["Applanga.SelectTag"].coordinate(withNormalizedOffset: CGVector(dx: 0.5, dy: 0.5)).tap()
+        app.tables.staticTexts["Settings"].tap();
+        app.buttons["Applanga.ConfirmScreenshot"].coordinate(withNormalizedOffset: CGVector(dx: 0.5, dy: 0.5)).tap()
+
+        predicate = NSPredicate(format: "exists == 1")
+        query = XCUIApplication().buttons["Applanga.SelectTag"];
+        expectation(for: predicate, evaluatedWith: query, handler: nil)
+        waitForExpectations(timeout: 30, handler: nil)
+        
+        app.buttons["Applanga.CancelScreenshot"].coordinate(withNormalizedOffset: CGVector(dx: 0.5, dy: 0.5)).tap()
+        app.buttons["Applanga.ToggleDraftMenu"].coordinate(withNormalizedOffset: CGVector(dx: 0.5, dy: 0.5)).tap()
         
         // nav
         hamburgerBtn.tap()
         sleep(1)
-        // ! take screenshot !
+        // take screenshot
+        app.buttons["Applanga.ToggleDraftMenu"].coordinate(withNormalizedOffset: CGVector(dx: 0.5, dy: 0.5)).tap()
+        app.buttons["Applanga.OpenScreenshotView"].coordinate(withNormalizedOffset: CGVector(dx: 0.5, dy: 0.5)).tap()
+        app.buttons["Applanga.SelectTag"].coordinate(withNormalizedOffset: CGVector(dx: 0.5, dy: 0.5)).tap()
+        app.tables.staticTexts["Navigation"].tap();
+        app.buttons["Applanga.ConfirmScreenshot"].coordinate(withNormalizedOffset: CGVector(dx: 0.5, dy: 0.5)).tap()
+
+        predicate = NSPredicate(format: "exists == 1")
+        query = XCUIApplication().buttons["Applanga.SelectTag"];
+        expectation(for: predicate, evaluatedWith: query, handler: nil)
+        waitForExpectations(timeout: 30, handler: nil)
+        
+        app.buttons["Applanga.CancelScreenshot"].coordinate(withNormalizedOffset: CGVector(dx: 0.5, dy: 0.5)).tap()
+        app.buttons["Applanga.ToggleDraftMenu"].coordinate(withNormalizedOffset: CGVector(dx: 0.5, dy: 0.5)).tap()
         
         sleep(1)
     }
@@ -89,18 +141,8 @@ class ios_sdkUITests: XCTestCase {
 
 }
 
-func takeScreenshots(language: String) {
-    checkAndChangeLanguage(language: language)
-    
-    
-}
-
 func checkAndChangeLanguage(language: String) {
     if (Locale.current.languageCode! != language) {
-        Applanga.setLanguage(language)
+        // go to settings and change language
     }
-}
-
-func captureScreenshot(app: XCUIApplication) {
-
 }
