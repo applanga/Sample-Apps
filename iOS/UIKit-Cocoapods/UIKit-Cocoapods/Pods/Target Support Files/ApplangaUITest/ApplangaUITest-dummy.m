a1532ac06e5e7a8c213b2b7756b3b6964256ee9d
Juan Santana <jbsantana2010@gmail.com>
2022-03-09 14:20:28 -0400

    initial commit

---
+++ iOS/UIKit-Cocoapods/UIKit-Cocoapods/Pods/Target Support Files/ApplangaUITest/ApplangaUITest-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_ApplangaUITest : NSObject
+@end
+@implementation PodsDummy_ApplangaUITest
+@end
---
+++ iOS/UIKit-Cocoapods/UIKit-Cocoapods/Pods/Target Support Files/Pods-WeatherSample-WeatherSampleUITests/Pods-WeatherSample-WeatherSampleUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_WeatherSample_WeatherSampleUITests : NSObject
+@end
+@implementation PodsDummy_Pods_WeatherSample_WeatherSampleUITests
+@end
